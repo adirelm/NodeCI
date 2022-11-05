f8078422b8abd4182e0f972c85ca44d17c7420fb
Adir Elmakais <104345864+AdirElmakais@users.noreply.github.com>
2022-11-05 15:08:11 +0200

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb://adir4455:1321997a@ac-t8iwukp-shard-00-00.fgmo6qg.mongodb.net:27017,ac-t8iwukp-shard-00-01.fgmo6qg.mongodb.net:27017,ac-t8iwukp-shard-00-02.fgmo6qg.mongodb.net:27017/blog-dev?ssl=true&replicaSet=atlas-e4v98l-shard-0&authSource=admin&retryWrites=true&w=majority",
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
+};
